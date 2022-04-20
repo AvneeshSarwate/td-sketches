3f4c8df5322aa8365a7be3bf6c9cdab480c5cf8a
Avneesh Sarwate <avneeshsarwate@gmail.com>
2022-04-20 14:29:44 -0400

    adding filters and step times to text warper, adding module grouping to auto-sliders

--- slider_auto_osc/index.module.js
+++ slider_auto_osc/index.module.js
@@ -27,28 +27,48 @@ function htmlToElement(html) {
  * @param {String} addr 
  * @param {String} val 
  */
-function sendSliderMessage(addr, val) {
-    websocket.send(JSON.stringify({addr, val}));
+function sendControlMessage(addr, val, parent, type) {
+    websocket.send(JSON.stringify({addr, val, parent, type}));
 }
 
 /**
  * @param {String} sliderName 
  */
-function createSlider(sliderName) {
+function createSlider(sliderName, containerName) {
+    const htmlId = sliderName + '_' + containerName;
     const sliderTemplate = `
     <div>
-        <span id=${sliderName}Container>
-            <input class="slider" id=${sliderName} type="range" min="0" max="1" step="0.001"> <label>${sliderName}<label> 
+        <span id=${htmlId}Container>
+            <input class="slider" id=${htmlId} type="range" min="0" max="1" step="0.001"> <label>${sliderName}<label> 
         </span>
         <br><br>
     </div> 
     `
 
     const singleContainer = htmlToElement(sliderTemplate);
-    slidersContainer.append(singleContainer);
-    const slider = document.getElementById(sliderName);
+    document.getElementById(containerName).append(singleContainer);
+    const slider = document.getElementById(htmlId);
     slider.oninput = function(val) {
-        sendSliderMessage(sliderName, val.currentTarget.value)
+        sendControlMessage(sliderName, val.currentTarget.value, containerName, 'slider');
+    }
+}
+
+function createButton(buttonName, containerName) {
+    const htmlId = buttonName + '_' + containerName;
+    const buttonTemplate = `
+    <div>
+        <span id=${htmlId}Container>
+            <input class="button" id=${htmlId} type="button" min="0" max="1" step="0.001"> <label>${buttonName}<label> 
+        </span>
+        <br><br>
+    </div> 
+    `
+
+    const singleContainer = htmlToElement(buttonTemplate);
+    document.getElementById(containerName).firstChild.append(singleContainer); //Buttons have a sub-container
+    const button = document.getElementById(htmlId);
+    button.oninput = function(val) {
+        sendControlMessage(buttonName, val.currentTarget.value, containerName, 'button');
     }
 }
 
@@ -68,6 +88,18 @@ function arrayEq(arr1, arr2) {
     return;
 }
 
+function splitSlidersByModule(slider_spec) {
+    const sliders_by_module = {};
+    slider_spec.forEach(spec => {
+        const module_name = spec[3].split("/")[2];
+        if(!sliders_by_module[module_name]) {
+            sliders_by_module[module_name] = [];
+        }
+        sliders_by_module[module_name].push(module_name);
+    });
+    return sliders_by_module;
+}
+
 websocket.onmessage= e => {
     console.log("ws message", e);
     const newControls = JSON.parse(e.data);
@@ -80,12 +112,31 @@ websocket.onmessage= e => {
                 sliders.push(slider_spec[0] + "_" + "u");
                 sliders.push(slider_spec[0] + "_" + "v");
             }
-
         }
     } )
     if(!arrayEq(newControls, controlList)) {
-        slidersContainer.innerHTML = "";
-        sliders.forEach(name => createSlider(name));
-        controlList = newControls;
+        // slidersContainer.innerHTML = "";
+        // sliders.forEach(name => createSlider(name));
+        // controlList = newControls;
+        const slidersByModule = splitSlidersByModule(slider_spec);
+        Object.keys(slidersByModule).forEach(mod_name => {
+            const mod_container = document.getElementById(mod_name);
+            mod_container.innerHTML = '<div class="buttons"></div>';
+
+            //split buttons and put them in a horizontal row at top of source module
+            slidersByModule[mod_name].filter(conf => conf[1] == "buttonCOMP").forEach(conf => {
+                createButton(conf[0], mod_name);
+            });
+
+            //make/splid sliders and add them under buttons
+            slidersByModule[mod_name].filter(conf => conf[1] == "buttonCOMP").forEach(conf => {
+                if(confg[2] == "u" || confg[2] == "v") {
+                    createSlider(confg[0], mod_name);
+                } else if(confg[2] == "uv") {
+                    createSlider(confg[0] + "_" + "u", mod_name);
+                    createSlider(confg[0] + "_" + "v", mod_name);
+                }
+            });
+        })
     }
 };
