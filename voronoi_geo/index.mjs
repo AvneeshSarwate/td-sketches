664df2483de2c27a2582564f5c23a0120a447fb3
Avneesh Sarwate <avneeshsarwate@gmail.com>
2022-12-06 14:05:05 -0500

    creating geometry out of voronoi cell calculated in javascript

--- voronoi_geo/index.mjs
+++ voronoi_geo/index.mjs
@@ -2,10 +2,31 @@ import { WebSocketServer } from 'ws';
 import { Voronoi } from "./rhill-voronoi-core.mjs";
 
 const wss = new WebSocketServer({ port: 8080 });
-
+const voronoi = new Voronoi();
+let diagram = null;
 wss.on('connection', function connection(ws) {
     ws.on('message', function message(data) {
-        console.log('received: %s', data);
+        const ptsArray = JSON.parse(data.toString());
+        const pts = ptsArray[0].map((e, i, arr) => ({ x: ptsArray[0][i], y: ptsArray[1][i] }));
+        if (diagram) voronoi.recycle(diagram);
+        diagram = voronoi.compute(pts, { xl: -1, xr: 1, yt: -1, yb: 1 })
+        // const polys = diagram.cells.map(c => {
+        //     return c.halfedges.map(h => {
+        //         const sp = h.getStartpoint();
+        //         return { x: sp.x, y: sp.y };
+        //     });
+        // });
+        const polys = diagram.cells.map(c => {
+            let x = [];
+            let y = []
+            c.halfedges.forEach(h => {
+                const sp = h.getStartpoint();
+                x.push(sp.x);
+                y.push(sp.y);
+            });
+            return [x, y];
+        });
+        ws.send(JSON.stringify(polys));
     });
 
     ws.send('something');
